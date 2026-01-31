96ba38546d4f4317aca01146f7710ebd8706589e
kvjohnny <kvjohnny.van@gmail.com>
2026-01-31 13:38:41 +0200

    fixed file in kt-count-monkeys task

--- src/main/kotlin/mate/academy/CountTheMonkeys.kt
+++ src/main/kotlin/mate/academy/CountTheMonkeys.kt
@@ -3,5 +3,10 @@ package mate.academy
 // Implement the `countTheMonkeys()` function so that it returns an IntArray
 // with elements from `1` to `count`.
 fun countTheMonkeys(count: Int): IntArray {
-
+    if (count == 0) emptyArray<Int>()
+    val monkeyArray = IntArray(count)
+    for (i in 0 until count) {
+        monkeyArray[i] = i + 1
+    }
+    return monkeyArray
 }
